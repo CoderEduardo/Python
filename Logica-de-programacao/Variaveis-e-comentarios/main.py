fbaa329f3fe9e3abdcc3b98ceb4a8c05e7ae43c4
Luis Eduardo <luiseduardo250305@gmail.com>
2024-02-22 15:30:36 -0300

    Um pouco mais sobre variáveis

--- Logica-de-programacao/Variaveis-e-comentarios/main.py
+++ Logica-de-programacao/Variaveis-e-comentarios/main.py
@@ -31,3 +31,19 @@ booleano = True #São variáveis verdadeiras ou falsas
 nenhum = None #Representa a ausência de valor
 
 print(string,inteiro,racional,complexo,lista,tupla,conjunto,dicionario,booleano,nenhum)
+
+#Para saber um tipo de variável basta usar o type()
+print(type(complexo))
+
+#Declarando varias variáveis ao mesmo tempo
+var1, var2, var3, var4 = "Texto1", "Texto2","Texto3","Texto4"
+print(var4)
+
+#Para atribuir o mesmo valor a varias variáveis diferentes fazemos o seguinte
+var5 = var6 = var7 = var8 = "Ana Paula"
+print(var5,var6,var7,var8) 
+
+#Se tiver uma coleção de valores em uma lista podemos extrair em variáveis, isso é chamado descompactar 
+exemplo = "Texto1", "Texto2","Texto3","Texto4"
+var9, var10, var11, var12 = exemplo 
+print(var9,var10,var11,var12)
