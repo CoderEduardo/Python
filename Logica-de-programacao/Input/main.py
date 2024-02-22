1086eb4a5a80bb4efee54c70d2a9794e696a485e
Luis Eduardo <luiseduardo250305@gmail.com>
2024-02-22 20:49:31 -0300

    Um pouco sobre input

---
+++ Logica-de-programacao/Input/main.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: \n")
+nota1 = float(input("Digite a nota 1: \n"))
+nota2 = float(input("Digite a nota 2:\n"))
+media = (nota1+nota2) / 2
+print(nome, "a sua média é de", media)
