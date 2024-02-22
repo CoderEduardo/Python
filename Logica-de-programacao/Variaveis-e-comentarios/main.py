475419f4d0a546cddfee4ffb6177c4964921293a
Luis Eduardo <luiseduardo250305@gmail.com>
2024-02-22 14:32:38 -0300

    Aprendendo sobre comentários e variáveis

---
+++ Logica-de-programacao/Variaveis-e-comentarios/main.py
@@ -0,0 +1,33 @@
+#Para fazer um comentário em python basta usar o o hashtag
+
+"""
+Outra forma de fazer comentários é usando as aspas, dessa forma conseguimos usar varias linhas
+"""
+
+#O que define o tipo da variavel e a forma que você declara ela
+
+#Variável String
+string = "Luis Eduardo"
+#Variável Int
+inteiro = 42 
+#Variável Float
+racional = 5.5
+#Variável Complex, usado para calculos especificos
+complexo = 5 + 5j
+
+#Variável list
+lista = [1,2,3]
+#Variável tuple
+tupla = (6,7,8)
+#A diferença entre list e tuple é que a list pode ser alterada depois de criada e a tuple não, ambas são ordenadas
+
+#Variável conjunto
+conjunto = {3,2,5}  #Lista não ordenada que não se repete
+#Variável dict
+dicionario = {"chave":"valor"} #Uma coleção não ordenada de pares chave-valor
+#Variável Booleano
+booleano = True #São variáveis verdadeiras ou falsas
+#Variável None
+nenhum = None #Representa a ausência de valor
+
+print(string,inteiro,racional,complexo,lista,tupla,conjunto,dicionario,booleano,nenhum)
