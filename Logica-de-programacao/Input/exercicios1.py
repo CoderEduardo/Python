9c94032b289caefd77458a1cf8688e48a1ce60e7
Luis Eduardo <luiseduardo250305@gmail.com>
2024-02-22 21:01:53 -0300

    Exercícios sobre input

---
+++ Logica-de-programacao/Input/exercicios1.py
@@ -0,0 +1,12 @@
+numero = int(input("A tabuada de qual número você deseja ver?\n"))
+
+print(numero, " * 1 = ", numero* 1 )
+print(numero, " * 2 = ", numero* 2 )
+print(numero, " * 3 = ", numero* 3 )
+print(numero, " * 4 = ", numero* 4 )
+print(numero, " * 5 = ", numero* 1 )
+print(numero, " * 6 = ", numero* 6 )
+print(numero, " * 7 = ", numero* 7 )
+print(numero, " * 8 = ", numero* 8 )
+print(numero, " * 9 = ", numero* 9 )
+print(numero, " * 10 = ", numero* 10 )
