01e4b043b2a8e42fa43e78a629f948be9a3cb7ff
Luis Eduardo <luiseduardo250305@gmail.com>
2024-03-02 00:11:32 -0300

    Usando a função anônima lambda

--- Logica-de-programacao/Funcoes/lambda.py
+++ Logica-de-programacao/Funcoes/lambda.py
@@ -3,3 +3,7 @@ print(dobrar(5))
 
 status = lambda n: "Negativo" if n < 0 else ("Zero" if n == 0 else "Positivo")
 print(status(-4))
+
+lista = [("João",35),("Maria",25),("Pedro",40)]
+pessoas_ordenadas = sorted(lista,key=lambda x:x[1])
+print(pessoas_ordenadas)
