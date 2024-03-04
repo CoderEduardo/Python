afbac4815e1332a92fb040d7e8fa0c33b9218ff9
Luis Eduardo <luiseduardo250305@gmail.com>
2024-03-04 01:24:41 -0300

    Introdução a listas

---
+++ Logica-de-programacao/Listas/main.py
@@ -0,0 +1,18 @@
+#Definindo uma lista
+notas_aluno = [95,70,80,65,99]
+print(notas_aluno)
+
+#Misturando diversos dados em uma lista
+misto = [10,56,"Luis Eduardo",89.99,True,["a","b","c"]]
+print(misto)
+
+#Uma lista nos permite misturar diversos tipos de dados
+
+#Para acessar uma lista começando pelo final usamos o -1, pegando o último item, e assim seguimos a sequência
+print(misto[-2])
+
+frutas = ["Maça","Banana","Cereja","Damasco","Figo"]
+print(f"Primeira fruta da lista: {frutas[0]}")
+print(f"Terceira fruta da lista {frutas[2]}")
+print(f"Última fruta da lista: {frutas[-1]}")
+print(f"Penultima fruta da lista: {frutas[-2]}")
