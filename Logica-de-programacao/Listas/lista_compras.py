7767d8cc31df6b2534eaa08e147e6e41741ed12d
Luis Eduardo <luiseduardo250305@gmail.com>
2024-08-23 14:08:28 -0300

    Refatorando o código, dividindo por funções

--- Logica-de-programacao/Listas/lista_compras.py
+++ Logica-de-programacao/Listas/lista_compras.py
@@ -13,44 +13,55 @@
 lista_compras = []
 entrada = True
 
+##funções
+def adicionarItem(args):
+    lista_compras.append(args)
+    print(f"O item {args} foi adicionado!!!")
+
+def removerItem(args):
+        existe = None
+        for elemento in lista_compras:
+                existe = True if elemento == args else False      
+        if existe:
+            lista_compras.remove(args)
+            print(f"O item {args} foi removido com sucesso")
+        else:
+            print(f"Não conseguimos apagar o item {args}, pois ele não estava cadastrador anteriormente")
+
+def mostrarItens(args):
+     if len(args) == 0:
+            print("A sua lista ainda está vazia!!!")
+     else:
+        print("Lista de compras")
+        for itens in args:
+            print(f" - {itens}")
+
 while entrada:
 
-    opcao = int(input("\n 1. Adicinar item à lista \n 2. Remover item da lista \n 3. Exibir lista de compras \n 4. Sair \n"))
+    opcao = str(input("\n 1. Adicinar item à lista \n 2. Remover item da lista \n 3. Exibir lista de compras \n 4. Sair \n"))
 
     ##Opcção que adiciona um noovo item
-    if opcao == 1:
+    if opcao == "1":
         item = str(input("Digite o nome do novo item: ")).lower()
-        lista_compras.append(item)
-        print(f"O item {item} foi adicionado!!!")
+        adicionarItem(item)
     
     ##Opção que remove um item
-    elif opcao == 2:
+    elif opcao == "2":
         item = str(input("Digite o nome do item que você gostaria de remover: ")).lower()
+        removerItem(item)
 
-        for elemento in lista_compras:
-             existe = True if elemento == item else False
-
-        if existe:
-            lista_compras.remove(item)
-            print(f"O item {item} foi removido com sucesso")
-        else:
-            print(f"Não conseguimos apagar o item {item}, pois ele não estava cadastrador anteriormente")
-        
     ##Opção que exibe lista de compras
-    elif opcao == 3:
-        if len(lista_compras) == 0:
-            print("A sua lista ainda está vazia!!!")
-        else:
-            for itens in lista_compras:
-                print("Lista de compras")
-                print(f" - {itens}")
+    elif opcao == "3":
+        mostrarItens(lista_compras)
     
     #Opção de saída
-    elif opcao == 4:
+    elif opcao == "4":
         break
 
     else:
         print("Digite uma opção válida!!!")
+
+
             
 
           
